8e29b0c3fdd5ae348fbb5b0c869624c061d0f586
mitul200 <mitul5267@gmail.com>
2024-08-28 12:42:50 +0600

    added normal bid page

---
+++ src/app/(authentication)/normal-bid/page.tsx
@@ -0,0 +1,12 @@
+import NormalBid from "@/components/normal-bid/NormalBid";
+import React from "react";
+
+const page = () => {
+  return (
+    <div>
+      <NormalBid />
+    </div>
+  );
+};
+
+export default page;
