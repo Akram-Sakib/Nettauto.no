7aac78465908589c72e56bf3b9620b75cf257133
akram-sakib <sayedakramsakib@gmail.com>
2024-08-30 18:10:46 +0600

    :bug: fixed count-achievement page responsiveness

--- src/components/homepage/count-achievement-section.tsx
+++ src/components/homepage/count-achievement-section.tsx
@@ -6,29 +6,29 @@ const CountAchievement = () => {
   return (
     <section className="py-20">
       <Container>
-        <div className="max-w-7xl rounded-tl-3xl rounded-br-3xl bg-[#06063E] relative px-20 grid grid-cols-1 md:grid-cols-2 xl:grid-cols-4 justify-between">
+        <div className="max-w-7xl rounded-tl-3xl rounded-br-3xl bg-[#06063E] relative px-20 grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 justify-between">
           <div className="text-white space-y-1 py-6 md:py-8 lg:py-10 text-center relative">
             <Image
               src={"/Layer 2.png"}
               alt="Car"
               width={176}
               height={115}
-              className="absolute w-[100px] h-[65px] md:w-[176px] md:h-[115px] bottom-24 lg:bottom-36 right-6"
+              className="absolute w-[100px] h-[65px] bottom-24 lg:bottom-28 lg:right-20 xl:w-32 xl:h-20 right-6"
             />
-            <h3 className="text-2xl md:text-3xl lg:text-[40px]">5000 +</h3>
-            <p className="text-lg md:text-xl lg:text-2xl">Biler solgt</p>
+            <h3 className="text-2xl md:text-2xl xl:text-[40px]">5000 +</h3>
+            <p className="text-lg md:text-xl xl:text-2xl">Biler solgt</p>
           </div>
           <div className="text-white space-y-2 py-4 md:py-8 lg:py-10 text-center">
-            <h3 className="text-2xl md:text-3xl lg:text-[40px]">500 +</h3>
-            <p className="text-lg md:text-xl lg:text-2xl">Kjøpere registrert</p>
+            <h3 className="text-2xl md:text-2xl xl:text-[40px]">500 +</h3>
+            <p className="text-lg md:text-xl xl:text-2xl">Kjøpere registrert</p>
           </div>
           <div className="text-white space-y-2 py-4 md:py-8 lg:py-10 text-center">
-            <h3 className="text-2xl md:text-3xl lg:text-[40px]">30,000+</h3>
-            <p className="text-lg md:text-xl lg:text-2xl">Innkommende bud</p>
+            <h3 className="text-2xl md:text-2xl xl:text-[40px]">30,000+</h3>
+            <p className="text-lg md:text-xl xl:text-2xl">Innkommende bud</p>
           </div>
           <div className="text-white space-y-2 py-4 md:py-8 lg:py-10 text-center">
-            <h3 className="text-2xl md:text-3xl lg:text-[40px]">5000+</h3>
-            <p className="text-lg md:text-xl lg:text-2xl">Fornøyde selgere</p>
+            <h3 className="text-2xl md:text-2xl xl:text-[40px]">5000+</h3>
+            <p className="text-lg md:text-xl xl:text-2xl">Fornøyde selgere</p>
           </div>
         </div>
         <div className="mt-10 md:mt-20 text-center">
